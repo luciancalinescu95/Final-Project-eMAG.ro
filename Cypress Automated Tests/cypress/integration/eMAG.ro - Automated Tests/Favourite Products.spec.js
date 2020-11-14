15b1760ac08ca1c74884097d99a916381e8b33b5
luciancalinescu95 <lucian.calinescu95@gmail.com>
2020-11-14 13:47:52 +0200

    Cypress

--- Cypress Automated Tests/cypress/integration/eMAG.ro - Automated Tests/Favourite Products.spec.js
+++ Cypress Automated Tests/cypress/integration/eMAG.ro - Automated Tests/Favourite Products.spec.js
@@ -7,7 +7,7 @@ describe("Favourite Products",()=>{
         cy.get('[data-id="1"] > .js-megamenu-list-department-link > .megamenu-list-department__department-name').click({force:true});
         cy.get('.emg-aside-links > :nth-child(2) > a').click({force:true});
         cy.get('.emg-menu-wrapper > :nth-child(1) > :nth-child(1) > a').click({force:true});
-        cy.get('[data-name="Telefon mobil Samsung Galaxy A51, Dual SIM, 128GB, 4GB RAM, 4G, Prism Blue"] > .card > .card-section-wrapper > .card-section-mid > .card-body > .product-title').click({force:true});
+        cy.get('#card_grid > div:nth-child(3) > div > div > div.card-section-mid > h2 > a').click({force:true});
         cy.get('.product-page-actions > .add-to-favorites').click();
         cy.get('#my_wishlist > span.jewel.jewel-info').should("contain","1");
 
@@ -17,20 +17,20 @@ describe("Favourite Products",()=>{
         cy.get('[data-id="1"] > .js-megamenu-list-department-link > .megamenu-list-department__department-name').click({force:true});
         cy.get('.emg-aside-links > :nth-child(2) > a').click({force:true});
         cy.get('.emg-menu-wrapper > :nth-child(1) > :nth-child(1) > a').click({force:true});
-        cy.get('[data-name="Telefon mobil Samsung Galaxy A51, Dual SIM, 128GB, 4GB RAM, 4G, Prism Blue"] > .card > .card-section-wrapper > .card-section-mid > .card-body > .product-title').click({force:true});
+        cy.get('#card_grid > div:nth-child(3) > div > div > div.card-section-mid > h2 > a').click({force:true});
         cy.get('.product-page-actions > .add-to-favorites').click();
         cy.get('#my_wishlist > .visible-lg-inline').click({force:true});
-        cy.get('.product-title > span').should("contain","Telefon mobil Samsung Galaxy A51, Dual SIM, 128GB, 4GB RAM, 4G, Prism Blue");
+        cy.get('.product-title > span').should("contain","Telefon mobil Apple iPhone 11, 128GB, Black");
 
           });
     it("TM-QA08-457:Favourite - Delete item",()=>{
         cy.get('[data-id="1"] > .js-megamenu-list-department-link > .megamenu-list-department__department-name').click({force:true});
         cy.get('.emg-aside-links > :nth-child(2) > a').click({force:true});
         cy.get('.emg-menu-wrapper > :nth-child(1) > :nth-child(1) > a').click({force:true});
-        cy.get('[data-name="Telefon mobil Samsung Galaxy A51, Dual SIM, 128GB, 4GB RAM, 4G, Prism Blue"] > .card > .card-section-wrapper > .card-section-mid > .card-body > .product-title').click({force:true});
+        cy.get('#card_grid > div:nth-child(3) > div > div > div.card-section-mid > h2 > a').click({force:true});
         cy.get('.product-page-actions > .add-to-favorites').click();
         cy.get('#my_wishlist > .visible-lg-inline').click({force:true});
-        cy.get('.product-title > span').should("contain","Telefon mobil Samsung Galaxy A51, Dual SIM, 128GB, 4GB RAM, 4G, Prism Blue");
+        cy.get('.product-title > span').should("contain","Telefon mobil Apple iPhone 11, 128GB, Black");
         cy.get('.hidden-sm').click({force:true});
         cy.get('.products-number').should("contain","0 produse");
         
